5ee3ba1b11a51511d2fcac350a397f478e3a7d8a
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-02-06 21:30:16 +0900

    Add files via upload

---
+++ BOJ/2576 BOJ.py
@@ -0,0 +1,13 @@
+nums = []
+
+for i in range(7):
+    a = int(input())
+    if a % 2 != 0:
+        nums.append(a)
+
+if len(nums) == 0:
+    print(-1)
+
+else:
+    print(sum(nums))
+    print(min(nums))
