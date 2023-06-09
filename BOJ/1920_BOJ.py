0bfb91221bb3ea2d2082700c3391c723ced55166
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-06-09 20:44:50 +0900

    Add files via upload

--- BOJ/1920_BOJ.py
+++ BOJ/1920_BOJ.py
@@ -1,6 +1,21 @@
-N = int(input())
-n_set = set(map(int, input().split()))
-M = int(input())
-m_list = list(map(int, input().split()))
+def binary_search(s, e, num1, j):
+    while s <= e:
+        mid = (s+e) // 2
 
-print(*[1 if i in n_set else 0 for i in m_list], sep="\n")
+        if num1[mid] == j:
+            return 1
+        elif num1[mid] < j:
+            s = mid+1
+        else:
+            e = mid-1
+    return 0
+
+n1 = int(input())
+num1 = list(map(int, input().split()))
+n2 = int(input())
+num2 = list(map(int, input().split()))
+
+num1.sort()
+
+for j in num2:
+    print(binary_search(0, n1-1, num1, j))
