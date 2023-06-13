de8aec22ace04a596e78c0f764f683038c291e71
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-06-13 23:56:25 +0900

    Add files via upload

--- BOJ/1620_BOJ.py
+++ BOJ/1620_BOJ.py
@@ -1,16 +1,15 @@
-import sys
-
-n1, n2 = map(int, sys.stdin.readline().split())
-d1 = {}
+n1, n2 = map(int, input().split())
+d = dict()
+d2 = dict()
 
 for i in range(1, n1+1):
-    name = sys.stdin.readline().rstrip()
-    d1[i] = name
-    d1[name] = i
+    name = input()
+    d[name] = i
+    d2[i] = name
     
 for j in range(n2):
-    result = sys.stdin.readline().rstrip()
+    result = input()
     if result.isdigit():
-        print(d1[int(result)])
+        print(d2.get(int(result)))
     else:
-        print(d1[result])
+        print(d.get(result))
