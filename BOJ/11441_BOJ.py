20778924d4bf6131a549d2803589294c1ca36011
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-06-22 23:07:48 +0900

    Add files via upload

---
+++ BOJ/11441_BOJ.py
@@ -0,0 +1,15 @@
+import sys
+
+n = int(sys.stdin.readline())
+li = list(map(int, sys.stdin.readline().split()))
+
+m = int(sys.stdin.readline())
+
+result = [0] * (len(li)+1)
+
+for i in range(len(li)):
+    result[i + 1] = result[i] + li[i]
+    
+for k in range(m):
+    n1, n2 = map(int, sys.stdin.readline().split())
+    print(result[n2]-result[n1-1])
