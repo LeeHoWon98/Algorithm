aafffe06a165987ebaf4feff8ec3a5889635235e
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-09-27 00:11:32 +0900

    Add files via upload

---
+++ BOJ/2490_BOJ.py
@@ -0,0 +1,11 @@
+for i in range(3):
+    s = input()
+
+    if s.count("0") == 1:
+        print("A")
+    elif s.count("0") == 2:
+        print("B")
+    elif s.count("0") == 3:
+        print("C")
+    elif s.count("0") == 4:
+        print("D")
