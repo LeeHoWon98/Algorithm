f3344b6bd2654342f06a272b9543d8a0d6211e09
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-10-08 22:36:16 +0900

    Add files via upload

---
+++ BOJ/4539_BOJ.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+for i in range(n):
+    n1 = list(input())
+
+    for j in range(1, len(n1)):
+        if int(n1[-j]) >= 5:
+            n1[-j+1] = str(int(n1[j-1])+1)
+        n1[j] = "0"
+        
+    print("".join(n1))
