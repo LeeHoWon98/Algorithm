e7c49063f43f7d4ed2d70acf571f9de3334df8d6
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-10-29 16:39:16 +0900

    Add files via upload

--- BOJ/10825_BOJ.py
+++ BOJ/10825_BOJ.py
@@ -1,12 +1,11 @@
 import sys
 
 n = int(sys.stdin.readline())
-
 result = []
 
 for i in range(n):
-    s1, s2, s3, s4 = sys.stdin.readline().split()
-    result.append([s1, s2, s3, s4])
+    name, n1, n2, n3 = sys.stdin.readline().split()
+    result.append([name, n1, n2, n3])
 
 result.sort(key=lambda x:(-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
