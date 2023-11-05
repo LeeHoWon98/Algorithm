0c78f0a833ad7d2114e0af8c63b49accc8a7b09d
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-11-05 15:59:03 +0900

    Add files via upload

--- BOJ/13414_BOJ.py
+++ BOJ/13414_BOJ.py
@@ -1,17 +1,18 @@
-import sys
-
-n1, n2 = map(int, sys.stdin.readline().split())
-
+n1, n2 = map(int, input().split())
 d = {}
+c = 0
 
 for i in range(n2):
-    s = sys.stdin.readline().rstrip()
-    d[s] = i
+    number = input()
+    if number in d:
+        del d[number]
+        d[number] = 1
+    else:
+        d[number] = 1
 
-d = sorted(d.items(), key=lambda x:x[1])
-
-if(n1 > len(d)):
-    n1 = len(d)
-
-for j in range(n1):
-    print(d[j][0])
+for k, v in d.items():
+    if c == n1:
+        break
+    else:
+        print(k)
+        c += 1
