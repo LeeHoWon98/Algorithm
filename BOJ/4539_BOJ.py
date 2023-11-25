4d4ed0cd4ea7140de47d5d9224f41b9e6c4c8767
이호원(범고래) <98515262+LeeHoWon98@users.noreply.github.com>
2023-11-26 01:42:46 +0900

    Add files via upload

--- BOJ/4539_BOJ.py
+++ BOJ/4539_BOJ.py
@@ -1,11 +1,8 @@
 n = int(input())
 
 for i in range(n):
-    n1 = list(input())
+    number = int(input())
+    for j in range(len(str(number))):
+        number = int(round(number+0.000001, -j))
 
-    for j in range(1, len(n1)):
-        if int(n1[-j]) >= 5:
-            n1[-j+1] = str(int(n1[j-1])+1)
-        n1[j] = "0"
-        
-    print("".join(n1))
+    print(number)
